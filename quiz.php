660b5a54c01c80744e5ce016cbffaf728594ce30
Sboragina <53148995+Sboragina@users.noreply.github.com>
2019-11-04 01:12:28 -0500

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -1,25 +1,14 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
+<!doctype html>
+<html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-
 <!-- Required meta tags -->
-
+<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="css/custom.css">
-<link rel="stylesheet" href="css/style-custom.css">
-<link rel="stylesheet" type="text/css" href="css/style.css" />
-</head>
-
-<body>
-
-	<div id="page-wrap">
-
-				
+<link rel="stylesheet" href="css/style-custom.css">			
 		
 <title>Final Quiz</title>
 </head>
