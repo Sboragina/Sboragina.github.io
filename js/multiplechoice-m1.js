92a8776710d84638f2958a3623515b1aecf71b9b
Sboragina <53148995+Sboragina@users.noreply.github.com>
2020-03-17 14:31:01 -0400

    Delete multiplechoice-m1.js

--- js/multiplechoice-m1.js
+++
@@ -1,8 +0,0 @@
-    set = "#module";
-
-    // You have just witnessed a car accident. What do you think you should do FIRST after witnessing a crime or collision like this?
-    $(set).find("input[name='question1']").on("click", function() {
-        question = ".question1", right = ["a"], wrong1 = ["b"], wrong2 = ["c"], wrong3 = ["d"], selection = [];
-        ConfigInstantQuiz(question);
-        CheckResultsMP(set);
-    });
